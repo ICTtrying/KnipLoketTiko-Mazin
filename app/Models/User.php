53931986dca0847b4015d6a0ab07700ce8c0ce50
wesley <340308@student.mboutrecht.nl>
2026-07-03 10:56:05 +0200

    feat: align user model to team schema

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,6 +17,10 @@ class User extends Authenticatable
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
+    protected $table = 'User';
+
+    protected $primaryKey = 'Id';
+
     /**
      * Get the attributes that should be cast.
      *
